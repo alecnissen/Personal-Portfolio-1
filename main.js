84ceeafb5b8ae6a6dae7b18c230bda9e4280d73a
alecnissen <alecnissen@yahoo.com>
2023-10-05 13:46:28 -0400

    added footer content incluing icons, text, and placed footer img right next to text using basic flexbox styling, next will add technologies used section right below my projects, which will include icons of all the different technologies I am familar with. After that I will begin styling header section

--- main.js
+++ main.js
@@ -10,3 +10,20 @@
 // link to the selected project, a preview/snapshot of the site, 
 // within the card is a title, p tags, and icons for links to the selected sites, 
 // 
+
+// footer section, 
+// card with title/header, a few p tags, and img, 
+
+// the goal should be to get the picture to line up at the footer, 
+// mostly get all the HTML and structure of the site down for the most part, 
+// try to get the img in the footer to line up with the text, just like the example, 
+
+// OK I now have the footer img right next to the footer text content 
+// I more or less have all the html content, 
+// before moving on and styling the header section first, 
+// add another container with logos of technologies used, 
+// download icons/logos for 
+// JS, HTML, CSS, SASS, REACT, JEST, GIT, GITHUB, NPM
+// 
+// under my projects, make a flex container and add img tags 
+// which will include icons for various technologies. 

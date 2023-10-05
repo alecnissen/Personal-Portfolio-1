0f42ff533590adc28a4e1891a27ced2d656f81d8
alecnissen <alecnissen@yahoo.com>
2023-10-04 22:44:36 -0400

    added basic html for projects section, will now do the same for footer content

---
+++ main.js
@@ -0,0 +1,12 @@
+// get all the HTML down, 
+// dont worry about things lining up exaclty, 
+// get the skeleton down of all sections, 
+// then we can begin to style the sections. 
+// 
+
+// main project section, 
+// container for all items, 
+// each item will have a card, 
+// link to the selected project, a preview/snapshot of the site, 
+// within the card is a title, p tags, and icons for links to the selected sites, 
+// 
